75d92f581d5db4aada8f9101ddd1414c77b0a13d
Daniely <dahhnyelyy@gmail.com>
2024-03-04 14:11:14 -0300

    senha ocultada

--- members/forms.py
+++ members/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
 from members.models import Usuario 
+from django import forms
 
 class UsuarioForms(ModelForm):
     class Meta:
         model= Usuario
         fields= ['email', 'senha']
+        widgets = {
+            'senha': forms.PasswordInput(),  # ocultar a senha
+    }
+
+
 
